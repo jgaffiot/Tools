4ccc815308e9ab7d3f8171cef5ecbbe78731c625
jgaffiot <jonathan.gaffiot@corsicasole.com>
2021-05-22 00:16:22 +0200

    Update test_Math

--- test/test_Math.py
+++ test/test_Math.py
@@ -10,69 +10,70 @@ def test_constant():
 
 def test_square():
     """Unitary test of tools::Math::sq(uare)"""
-    m = PyTools.tools.Math
-    assert m.sq(0) == 0
-    assert m.sq(1) == 1
-    assert m.sq(2) == 4
-    assert m.sq(1000) == 1000000
-    assert m.square(10) == 100
+    sq = PyTools.tools.Math.sq
+    square = PyTools.tools.Math.sq
+    assert sq(0) == 0
+    assert sq(1) == 1
+    assert sq(2) == 4
+    assert sq(1000) == 1000000
+    assert square(10) == 100
 
 
 def test_cube():
     """Unitary test of tools::Math::cube"""
-    m = PyTools.tools.Math
-    assert m.cube(0) == 0
-    assert m.cube(1) == 1
-    assert m.cube(2) == 8
-    assert m.cube(10) == 1000
+    cube = PyTools.tools.Math.cube
+    assert cube(0) == 0
+    assert cube(1) == 1
+    assert cube(2) == 8
+    assert cube(10) == 1000
 
 
 def test_pow_n():
     """Unitary test of tools::Math::pow_n"""
-    m = PyTools.tools.Math
-    assert m.pow_n(0, 0) == 1
-    assert m.pow_n(42, 0) == 1
-    assert m.pow_n(0, 1) == 0
-    assert m.pow_n(0, 42) == 0
-    assert m.pow_n(1, 1) == 1
-    assert m.pow_n(1, 42) == 1
-    assert m.pow_n(2, 0) == 1
-    assert m.pow_n(2, 1) == 2
-    assert m.pow_n(2, 2) == 4
-    assert m.pow_n(2, 3) == 8
-    assert m.pow_n(2, 10) == 1024
-    assert m.pow_n(10, 6) == 1000000
+    pow_n = PyTools.tools.Math.pow_n
+    assert pow_n(0, 0) == 1
+    assert pow_n(42, 0) == 1
+    assert pow_n(0, 1) == 0
+    assert pow_n(0, 42) == 0
+    assert pow_n(1, 1) == 1
+    assert pow_n(1, 42) == 1
+    assert pow_n(2, 0) == 1
+    assert pow_n(2, 1) == 2
+    assert pow_n(2, 2) == 4
+    assert pow_n(2, 3) == 8
+    assert pow_n(2, 10) == 1024
+    assert pow_n(10, 6) == 1000000
 
 
 def test_sum2():
     """Unitary test of tools::Math::sum2"""
-    m = PyTools.tools.Math
-    assert m.sum2(0, 0) == 0
-    assert m.sum2(0, 10) == 100
-    assert m.sum2(10, 0) == 100
-    assert m.sum2(1, 1) == 2
-    assert m.sum2(2, 2) == 8
-    assert m.sum2(10, 10) == 200
-    assert m.sum2(10, 1) == 101
+    sum2 = PyTools.tools.Math.sum2
+    assert sum2(0, 0) == 0
+    assert sum2(0, 10) == 100
+    assert sum2(10, 0) == 100
+    assert sum2(1, 1) == 2
+    assert sum2(2, 2) == 8
+    assert sum2(10, 10) == 200
+    assert sum2(10, 1) == 101
 
 
 def test_norm2():
     """Unitary test of tools::Math::norm2"""
-    m = PyTools.tools.Math
-    assert m.norm2(0, 0) == 0
-    assert m.norm2(0, 10) == 10
-    assert m.norm2(10, 0) == 10
-    assert m.norm2(1, 1) == m.Sqrt2()
-    assert m.norm2(3, 4) == 5
+    norm2 = PyTools.tools.Math.norm2
+    assert norm2(0, 0) == 0
+    assert norm2(0, 10) == 10
+    assert norm2(10, 0) == 10
+    assert norm2(1, 1) == PyTools.tools.Math.Sqrt2()
+    assert norm2(3, 4) == 5
 
 
 def test_diff2():
     """Unitary test of tools::Math::diff2"""
-    m = PyTools.tools.Math
-    assert m.diff2(0, 0) == 0
-    assert m.diff2(42, 42) == 0
-    assert m.diff2(10, 0) == 100
-    assert m.diff2(0, 10) == -100
-    assert m.diff2(1, 2) == -3
-    assert m.diff2(1, 10) == -99
-    assert m.diff2(4, 5) == -9
+    diff2 = PyTools.tools.Math.diff2
+    assert diff2(0, 0) == 0
+    assert diff2(42, 42) == 0
+    assert diff2(10, 0) == 100
+    assert diff2(0, 10) == -100
+    assert diff2(1, 2) == -3
+    assert diff2(1, 10) == -99
+    assert diff2(4, 5) == -9
