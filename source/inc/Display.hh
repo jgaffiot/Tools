cd7a079d0fe03ffd50e926b6fd3bfcd1fc773666
Jonathan Gaffiot <jonathan.gaffiot@external.thalesgroup.com>
2021-07-22 14:22:21 +0200

    Format the code

--- source/inc/Display.hh
+++ source/inc/Display.hh
@@ -14,15 +14,20 @@
 
 namespace tools
 {
-template<typename T> constexpr std::string_view type_name();
+template<typename T>
+constexpr std::string_view type_name();
 
-template<typename T=void> constexpr std::string_view type_name<void>() { return "void"; }
+template<typename T = void>
+constexpr std::string_view type_name<void>() {
+    return "void";
+}
 
-namespace detail_display {
+namespace detail_display
+{
 using type_name_prober = void;
 
-template<typename T> constexpr std::string_view wrapped_type_name()
-{
+template<typename T>
+constexpr std::string_view wrapped_type_name() {
 #ifdef __clang__
     return __PRETTY_FUNCTION__;
 #elif defined(__GNUC__)
@@ -30,42 +35,37 @@ template<typename T> constexpr std::string_view wrapped_type_name()
 #elif defined(_MSC_VER)
     return __FUNCSIG__;
 #else
-#error "Unsupported compiler"
+#    error "Unsupported compiler"
 #endif
 }
 
-constexpr std::size_t wrapped_type_name_prefix_length()
-{
+constexpr std::size_t wrapped_type_name_prefix_length() {
     return wrapped_type_name<type_name_prober>().find(type_name<type_name_prober>());
 }
 
-constexpr std::size_t wrapped_type_name_suffix_length()
-{
+constexpr std::size_t wrapped_type_name_suffix_length() {
     return wrapped_type_name<type_name_prober>().length()
-        - wrapped_type_name_prefix_length()
-        - type_name<type_name_prober>().length();
+           - wrapped_type_name_prefix_length() - type_name<type_name_prober>().length();
 }
 
-} // namespace detail_display
-
+}  // namespace detail_display
 
 //! Return the type name as a string, to log or debug
-template<typename T> constexpr std::string_view type_name(){
+template<typename T>
+constexpr std::string_view type_name() {
     constexpr auto wrapped_name = detail_display::wrapped_type_name<T>();
     constexpr auto prefix_length = detail_display::wrapped_type_name_prefix_length();
     constexpr auto suffix_length = detail_display::wrapped_type_name_suffix_length();
-    constexpr auto type_name_length = wrapped_name.length() - prefix_length - suffix_length;
+    constexpr auto type_name_length =
+        wrapped_name.length() - prefix_length - suffix_length;
     return wrapped_name.substr(prefix_length, type_name_length);
 }
 
 //! An operator<< on all enum types, enabling easy printing to log or debug
-template<typename Enum> inline std::ostream& operator<<(
-    typename std::enable_if<
-        std::is_enum<Enum>::value,
-        std::ostream
-    >::type& stream,
-    const Enum& e)
-{
+template<typename Enum>
+inline std::ostream& operator<<(
+    typename std::enable_if<std::is_enum<Enum>::value, std::ostream>::type& stream,
+    const Enum& e) {
     return stream << static_cast<typename std::underlying_type<Enum>::type>(e);
 }
 
