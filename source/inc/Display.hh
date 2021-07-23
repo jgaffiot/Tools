910a88822317520d8194b39cbc685b9a220fc4e6
Jonathan Gaffiot <jonathan.gaffiot@external.thalesgroup.com>
2021-07-23 09:23:15 +0200

    Cite initial authors

--- source/inc/Display.hh
+++ source/inc/Display.hh
@@ -1,6 +1,8 @@
 // --------------------------------------------------------------------------//
 /**
  * AUTHOR: J. Gaffiot
+ * FROM: Howard Hinnant, 康桓瑋, Val, einpoklum
+ * See https://stackoverflow.com/questions/81870/is-it-possible-to-print-a-variables-type-in-standard-c
  * DATE: 2021-07-22
  * CONTACT: j.gaffiot@laposte.net
  */
